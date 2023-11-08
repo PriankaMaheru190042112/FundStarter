0c2506087aa548d2e238cc61a667c79d83820aa7
PriankaMaheru190042112 <priankamaheru@iut-dhaka.edu>
2023-11-08 18:45:21 +0600

    writing functions for crowdfunding, fetched contracts

---
+++ fundstarter/context/content.js
@@ -0,0 +1,5 @@
+import crowdFunding from "./CrowdFunding.json";
+
+//crowd funding marketplace
+export const CrowdFundingAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+export const CrowdFundingABI= crowdFunding.abi
--- fundstarter/pages/_app.js
+++ fundstarter/pages/_app.js
@@ -2,13 +2,16 @@ import '@/styles/globals.css'
 
 import Header from '@/components/header'
 import Footer from '@/components/footer'
+import {CrowdFundingProvider} from '@/context/CrowdFunding'
 
 export default function App({ Component, pageProps }) {
   return(
   <>
+    <CrowdFundingProvider>
     <Header/>
     <Component {...pageProps} />
     <Footer/>
+    </CrowdFundingProvider>
   </>
   )
 }
